ee87fd4ab278825f3d8445492352b158c8b69849
Steven Pack <steven.pack.code@gmail.com>
2020-08-29 22:32:01 -0700

    Changed some &String to &str to remove the need for to_string() conversions

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
       continue;
     } 
 
-    last_asn_str = asn_str.to_string();    
+    last_asn_str = asn_str;    
     match parse_asn(&last_asn_str) {
       Ok(as_name) => println!("-> {}", as_name),
       Err(e) => eprintln!("{}", e)
@@ -49,21 +49,21 @@ fn read_line(line: Result<String, Error>) -> String {
   }
 }
 
-fn check_no_response(line: &String) -> Option<String> {
+fn check_no_response(line: &str) -> Option<&str> {
   if line.contains("*") {
-    return Some("-> *".to_string())
+    return Some("-> *")
   }
   None
 }
 
-fn check_reserved(line: &String) -> Option<String> {
+fn check_reserved(line: &str) -> Option<&str> {
   if line.contains("[AS0]") || line.contains("[AS?]") {    
-    return Some("-> AS0 (Reserved)".to_string());
+    return Some("-> AS0 (Reserved)")
   } 
   None  
 }
 
-fn parse_asn(asn_str: &String) -> Result<String, Error> {
+fn parse_asn(asn_str: &str) -> Result<String, Error> {
   //Convert to anumber
   let num_str = asn_str.replace("AS", "");
   let asn: AsNumber = match num_str.parse::<u32>() {
@@ -84,7 +84,7 @@ fn parse_asn(asn_str: &String) -> Result<String, Error> {
   Ok(as_name.to_string())  
 }
 
-fn get_asn_str(line: &String) -> Option<String> {
+fn get_asn_str(line: &str) -> Option<String> {
    //Look for [ASXXX]. Error if it's not there
    let start_index  = line.find("[").unwrap_or(usize::MAX);
    let end_index = line.find("]").unwrap_or(usize::MAX);
@@ -112,7 +112,7 @@ mod tests {
   #[test]
   fn check_no_response_msg() {
     let test_line_no_response = " 8  * * *";
-    if let Some(msg) = check_no_response(&test_line_no_response.to_string()) {
+    if let Some(msg) = check_no_response(&test_line_no_response) {
       assert_eq!(msg, "-> *")
     } else {
       assert!(false, "Expected the no response message");
@@ -122,7 +122,7 @@ mod tests {
   #[test]
   fn check_reserved_msg() {
     let test_line = "1  [AS0] 192.168.8.1 (192.168.8.1)  3.722 ms  4.705 ms  4.613 ms";
-    if let Some(msg) = check_reserved(&test_line.to_string()) {
+    if let Some(msg) = check_reserved(&test_line) {
       assert_eq!(msg, "-> AS0 (Reserved)")
     } else {
       assert!(false, "Expected the reserved message");
@@ -132,27 +132,27 @@ mod tests {
   #[test]
   fn get_asn_str() {
     let test_line = "12  [AS13335] 172.67.6.216 (172.67.6.216)  17.510 ms  16.734 ms  15.266 ms";
-    let asn_str = super::get_asn_str(&test_line.to_string());
+    let asn_str = super::get_asn_str(&test_line);
     assert!(asn_str.is_some(), "Expected a result");
     assert_eq!(asn_str.unwrap(), "AS13335", "Expected the right ASN");
   }
 
   #[test]
   fn parse_asn_fail() {
-    let result = parse_asn(&"ASXXX".to_string());
+    let result = parse_asn(&"ASXXX");
     assert!(result.is_err(), "Didn't expect to parse ASXXXX");      
   }
 
   #[test]
   fn parse_asn_not_found() {
-    let result = parse_asn(&"AS111111".to_string());
+    let result = parse_asn(&"AS111111");
     println!("{:?}", result);
     assert!(result.is_err(), "Didn't expect to parse AS9999");      
   }
 
   #[test]
   fn parse_asn_ok() {
-    let result = parse_asn(&"AS13335".to_string());
+    let result = parse_asn(&"AS13335");
     println!("{:?}", result);
     assert!(result.is_ok(), "Cloudflare ASN lookup should succeed");
     assert!(result.unwrap().to_uppercase().contains("CLOUDFLARE"), "Expected CLOUDFLARE in the name");
